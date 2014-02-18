4f68735d6521a898621044f4c39a8f3681554263
stefanmaric <alexstefan92@gmail.com>
2014-02-18 00:16:20 -0430

    Replaced Phantomjs+Casperjs with request+cheerio+async due performance issues. Added package.json

--- utils.js
+++
@@ -1,9 +0,0 @@
-exports.getColoredStatus = function(casper) {
-  var status = casper.status().currentHTTPStatus;
-  switch(status) {
-    case 200: var statusStyle = { fg: 'green', bold: true }; break;
-    case 404: var statusStyle = { fg: 'red', bold: true }; break;
-    default: var statusStyle = { fg: 'magenta', bold: true }; break;
-  }
-  return casper.colorizer.format(status, statusStyle)
-}
