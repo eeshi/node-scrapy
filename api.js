b76e5ac171e4c4adc3ae9340f721aeee85aba773
stefanmaric <alexstefan92@gmail.com>
2014-04-02 12:12:24 -0430

    Callbacks structure and request call.

--- api.js
+++ api.js
@@ -2,9 +2,64 @@ var request = require('request');
 var cheerio = require('cheerio');
 var api = {};
 
+api.scrap = scrap;
 module.exports = api;
 
-api.scrap = function(url, model, options, callback) {
+
+function scrap(url, model, options, callback) {
+
+  var reqOptions = {};
+
+  if (typeof options.request != 'undefined') {
+    reqOptions = options.request;
+  }
+
+  reqOptions.uri = url;
+
+  getBody(reqOptions, function(err, data) {
+
+    if (err) {
+      return callback(err);
+    }
+
+    parseBody(data.body, model, function(err, data) {
+
+      if (err) {
+        return callback(err);
+      }
+
+      return callback(null, data);  
+
+    });
+  });
+};
+
+
+function getBody(options, callback) {
+
+  var data = {};
+
+  request(options, function(err, res, body) {
+
+    if (err) {
+      return callback(err);
+    }
+
+    if (res.statusCode == 200) {
+
+      data.res = res;
+      data.body = body;
+
+      return callback(null, data);
+
+    } else {
+      return callback({ msg: "NOT OK response.", res: res });
+    }
+
+  });
+}
+
+function parseBody(body, model, callback) {
 
   var err = null;
   var data = {};
@@ -13,6 +68,9 @@ api.scrap = function(url, model, options, callback) {
     return callback(err);
   }
 
-  return callback(null, data)
+  var $ = cheerio.load(body);
 
-};
+
+  return callback(null, data);
+
+}
