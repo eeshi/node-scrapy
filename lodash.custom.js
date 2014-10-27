fb5b7cd1f87af96e458eab785bfab117efa2951b
Stefan Maric <alexstefan92@gmail.com>
2014-10-26 22:38:54 -0430

    Fixing wrong custom build of lodash.

--- lodash.custom.js
+++ lodash.custom.js
@@ -1,7 +1,7 @@
 /**
  * @license
  * Lo-Dash 2.4.1 (Custom Build) <http://lodash.com/>
- * Build: `lodash modern exports="node" include="clone,partialRight,merge,mixin"`
+ * Build: `lodash modern exports="node" include="cloneDeep,partialRight,merge,mixin"`
  * Copyright 2012-2013 The Dojo Foundation <http://dojofoundation.org/>
  * Based on Underscore.js 1.5.2 <http://underscorejs.org/LICENSE>
  * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
@@ -892,20 +892,23 @@
   };
 
   /**
-   * Creates a clone of `value`. If `isDeep` is `true` nested objects will also
-   * be cloned, otherwise they will be assigned by reference. If a callback
-   * is provided it will be executed to produce the cloned values. If the
-   * callback returns `undefined` cloning will be handled by the method instead.
-   * The callback is bound to `thisArg` and invoked with one argument; (value).
+   * Creates a deep clone of `value`. If a callback is provided it will be
+   * executed to produce the cloned values. If the callback returns `undefined`
+   * cloning will be handled by the method instead. The callback is bound to
+   * `thisArg` and invoked with one argument; (value).
+   *
+   * Note: This method is loosely based on the structured clone algorithm. Functions
+   * and DOM nodes are **not** cloned. The enumerable properties of `arguments` objects and
+   * objects created by constructors other than `Object` are cloned to plain `Object` objects.
+   * See http://www.w3.org/TR/html5/infrastructure.html#internal-structured-cloning-algorithm.
    *
    * @static
    * @memberOf _
    * @category Objects
-   * @param {*} value The value to clone.
-   * @param {boolean} [isDeep=false] Specify a deep clone.
+   * @param {*} value The value to deep clone.
    * @param {Function} [callback] The function to customize cloning values.
    * @param {*} [thisArg] The `this` binding of `callback`.
-   * @returns {*} Returns the cloned value.
+   * @returns {*} Returns the deep cloned value.
    * @example
    *
    * var characters = [
@@ -913,33 +916,24 @@
    *   { 'name': 'fred',   'age': 40 }
    * ];
    *
-   * var shallow = _.clone(characters);
-   * shallow[0] === characters[0];
-   * // => true
-   *
-   * var deep = _.clone(characters, true);
+   * var deep = _.cloneDeep(characters);
    * deep[0] === characters[0];
    * // => false
    *
-   * _.mixin({
-   *   'clone': _.partialRight(_.clone, function(value) {
-   *     return _.isElement(value) ? value.cloneNode(false) : undefined;
-   *   })
+   * var view = {
+   *   'label': 'docs',
+   *   'node': element
+   * };
+   *
+   * var clone = _.cloneDeep(view, function(value) {
+   *   return _.isElement(value) ? value.cloneNode(true) : undefined;
    * });
    *
-   * var clone = _.clone(document.body);
-   * clone.childNodes.length;
-   * // => 0
+   * clone.node == view.node;
+   * // => false
    */
-  function clone(value, isDeep, callback, thisArg) {
-    // allows working with "Collections" methods without using their `index`
-    // and `collection` arguments for `isDeep` and `callback`
-    if (typeof isDeep != 'boolean' && isDeep != null) {
-      thisArg = callback;
-      callback = isDeep;
-      isDeep = false;
-    }
-    return baseClone(value, isDeep, typeof callback == 'function' && baseCreateCallback(callback, thisArg, 1));
+  function cloneDeep(value, callback, thisArg) {
+    return baseClone(value, true, typeof callback == 'function' && baseCreateCallback(callback, thisArg, 1));
   }
 
   /**
@@ -1539,8 +1533,7 @@
 
   /*--------------------------------------------------------------------------*/
 
-  // add functions that return unwrapped values when chaining
-  lodash.clone = clone;
+  lodash.cloneDeep = cloneDeep;
   lodash.identity = identity;
   lodash.isArray = isArray;
   lodash.isFunction = isFunction;
