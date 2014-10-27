66b14dbeebcdd3cbea5ef6b414d6f72f084d2c38
Stefan Maric <alexstefan92@gmail.com>
2014-10-26 19:34:39 -0430

    Deleting trash code.

--- index.js
+++ index.js
@@ -245,15 +245,3 @@ function parseItem(item, options, cb) {
   return cb(null, data);
 
 }
-
-function parseCookies(request) {
-    var list = {},
-        rc = request.headers.cookie;
-
-    rc && rc.split(';').forEach(function(cookie) {
-        var parts = cookie.split('=');
-        list[parts.shift().trim()] = unescape(parts.join('='));
-    });
-
-    return list;
-}
