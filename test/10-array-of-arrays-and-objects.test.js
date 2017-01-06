b4a5110b0ad08d2a00e730300f33525372650d00
Stefan Maric <me@stefanmaric.com>
2017-01-05 22:48:17 -0400

    Add test for mixed nested arrays and objects

---
+++ test/10-array-of-arrays-and-objects.test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let fs = require('fs')
+let path = require('path')
+
+let testName = path.basename(__filename, '.test.js')
+
+let source = fs.readFileSync(path.join(__dirname, testName + '.source.html'), 'utf8')
+let model = require(path.join(__dirname, testName + '.model'))
+let expected = require(path.join(__dirname, testName + '.result'))
+
+let test = require('tap').test
+let scrapy = require('..')
+
+test('arrays and objects in the model can be nested at any level', t => {
+  let result = scrapy.extract(source, model)
+  t.deepEqual(result, expected)
+  t.end()
+})
