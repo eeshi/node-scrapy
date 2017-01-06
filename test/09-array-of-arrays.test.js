3308b6db6788d8b405030ac69ef34120d7ac266e
Stefan Maric <me@stefanmaric.com>
2017-01-05 22:01:50 -0400

    Add test for array of arrays spec

---
+++ test/09-array-of-arrays.test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let fs = require('fs')
+let path = require('path')
+
+let testName = path.basename(__filename, '.test.js')
+
+let source = fs.readFileSync(path.join(__dirname, testName + '.source.html'), 'utf8')
+let model = require(path.join(__dirname, testName + '.model'))
+let expected = require(path.join(__dirname, testName + '.result'))
+
+let test = require('tap').test
+let scrapy = require('..')
+
+test('arrays with an array as second element should be treated as nested context', t => {
+  let result = scrapy.extract(source, model)
+  t.deepEqual(result, expected)
+  t.end()
+})
