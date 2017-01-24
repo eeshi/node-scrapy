12b199203445d16951023ebcd228c07aa534722f
Stefan Maric <me@stefanmaric.com>
2017-01-24 06:20:47 -0500

    Add initial spec test for getter query

---
+++ test/extract/11-getters.test.js
@@ -0,0 +1,37 @@
+'use strict'
+
+let fs = require('fs')
+let path = require('path')
+
+let testName = path.basename(__filename, '.test.js')
+
+let source = fs.readFileSync(path.join(__dirname, testName + '.source.html'), 'utf8')
+let model = require(path.join(__dirname, testName + '.model'))
+let expected = require(path.join(__dirname, testName + '.result'))
+
+let test = require('tap').test
+let scrapy = require('../..')
+
+let result = scrapy.extract(source, model)
+
+test('default getter should be $textContent', t => {
+  t.strictSame(result.default, expected.default)
+  t.strictSame(result.default, result.textContent)
+  t.end()
+})
+
+test('$text should be an alias of $textContent', t => {
+  t.strictSame(result.text, expected.text)
+  t.strictSame(result.text, result.textContent)
+  t.end()
+})
+
+test('$textNodes should only count for direct child nodes', t => {
+  t.strictSame(result.textNodes, expected.textNodes)
+  t.end()
+})
+
+test('should return matching attribute if getter is not one of the getters collection', t => {
+  t.same(result.links, expected.links)
+  t.end()
+})
