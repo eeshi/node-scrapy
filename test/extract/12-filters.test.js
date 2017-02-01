2061079af7843c806f9508948b0a9af07fc6523e
Stefan Maric <me@stefanmaric.com>
2017-02-01 13:13:09 +0100

    Add initial spec tests for trim filter

--- test/extract/12-filters.test.js
+++ test/extract/12-filters.test.js
@@ -23,3 +23,24 @@ test('normalizeWhitespace filter should should keep single whitespace ocurrences
   t.strictSame(result.properWhitespace, expected.properWhitespace)
   t.end()
 })
+
+test('trim:right should remove trailing whitespace, and leading whitespace should be preserved', t => {
+  t.strictSame(result.trimRight, expected.trimRight)
+  t.end()
+})
+
+test('trim:left should remove leading whitespace, and trailing whitespace should be preserved', t => {
+  t.strictSame(result.trimLeft, expected.trimLeft)
+  t.end()
+})
+
+test('trim:both should remove all leading and trailing whitespace', t => {
+  t.strictSame(result.trimBoth, expected.trimBoth)
+  t.end()
+})
+
+test('trim without arguments should default to trim:both', t => {
+  t.strictSame(result.trimAlone, expected.trimAlone)
+  t.strictSame(result.trimAlone, result.trimBoth)
+  t.end()
+})
