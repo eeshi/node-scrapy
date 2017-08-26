02666880e023fd97ccb3e92aafbacb9be6df06e6
Stefan Maric <me@stefanmaric.com>
2017-08-26 19:22:41 -0300

    Add little test util to import fixture sets
    To reduce test files' size.

---
+++ test/test-utils.js
@@ -0,0 +1,18 @@
+/* eslint-disable global-require, import/no-dynamic-require */
+
+const fs = require('fs')
+const path = require('path')
+
+module.exports = {
+  getFixtureSet
+}
+
+function getFixtureSet (dirName, fileName) {
+  const testName = path.basename(fileName, '.test.js')
+
+  return {
+    source: fs.readFileSync(path.join(dirName, `${testName}.source.html`), 'utf8'),
+    model: require(path.join(dirName, `${testName}.model`)),
+    expected: require(path.join(dirName, `${testName}.result`))
+  }
+}
