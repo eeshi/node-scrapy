83029236c21955ab817382515dc0fb893318e0c8
Stefan Maric <me@stefanmaric.com>
2019-01-13 14:25:38 +0100

    Add parsing options, normalize exports, and split code

--- test/query-parser.test.js
+++ test/query-parser.test.js
@@ -1,44 +1,44 @@
 const { test } = require('tap')
-const parse = require('../lib/query-parser')
+const { parseQuery } = require('../lib/query-parser')
 
 test('should extract CSS selector from query and keep it intact', (t) => {
   const query = '.home li > a'
-  const result = parse(query)
+  const result = parseQuery(query)
   t.strictSame(result.selector, '.home li > a')
   t.end()
 })
 
 test('should extract CSS selector from query and keep it intact, even if getter query is present', (t) => {
   const query = '.home li > a => $text'
-  const result = parse(query)
+  const result = parseQuery(query)
   t.strictSame(result.selector, '.home li > a')
   t.end()
 })
 
 test('should extract CSS selector from query and keep it intact, even if filter query is present', (t) => {
   const query = '.home li > a | trim:both'
-  const result = parse(query)
+  const result = parseQuery(query)
   t.strictSame(result.selector, '.home li > a')
   t.end()
 })
 
 test('should extract CSS selector from query and keep it intact, even if getter and filter queries are present', (t) => {
   const query = '.home li > a => $text | trim:both'
-  const result = parse(query)
+  const result = parseQuery(query)
   t.strictSame(result.selector, '.home li > a')
   t.end()
 })
 
 test('should return null for `getter` if not present in the query', (t) => {
   const query = '.home li > a'
-  const result = parse(query)
+  const result = parseQuery(query)
   t.strictSame(result.getter, null)
   t.end()
 })
 
 test('should extract `getter` from query if present', (t) => {
   const query = '.home li > a => href'
-  const result = parse(query)
+  const result = parseQuery(query)
   t.strictSame(result.getter, 'href')
   t.end()
 })
@@ -46,28 +46,28 @@ test('should extract `getter` from query if present', (t) => {
 test('should ignore whitespace around and in between `getter`', (t) => {
   const query = `.home li > a =>   hr e
     f  `
-  const result = parse(query)
+  const result = parseQuery(query)
   t.strictSame(result.getter, 'href')
   t.end()
 })
 
 test('should return empty array for `filters` if not present in the query', (t) => {
   const query = '.home li > a'
-  const result = parse(query)
+  const result = parseQuery(query)
   t.same(result.filters, [])
   t.end()
 })
 
 test('should extract filter name', (t) => {
   const query = '.home li > a | trim'
-  const result = parse(query)
+  const result = parseQuery(query)
   t.strictSame(result.filters[0].name, 'trim')
   t.end()
 })
 
 test('should extract all filter names in order', (t) => {
   const query = '.home li > a | trim | normalizeWhitespace'
-  const result = parse(query)
+  const result = parseQuery(query)
   t.strictSame(result.filters[0].name, 'trim')
   t.strictSame(result.filters[1].name, 'normalizeWhitespace')
   t.end()
@@ -75,7 +75,7 @@ test('should extract all filter names in order', (t) => {
 
 test("should extract filters' arguments as strings", (t) => {
   const query = '.home li > a | trim:right | someFilter:true | anotherFilter:test'
-  const result = parse(query)
+  const result = parseQuery(query)
   t.strictSame(result.filters[0].args[0], 'right')
   t.strictSame(result.filters[1].args[0], 'true')
   t.strictSame(result.filters[2].args[0], 'test')
@@ -84,14 +84,14 @@ test("should extract filters' arguments as strings", (t) => {
 
 test('should return empty array for filter arguments if none provided', (t) => {
   const query = '.home li > a | trim'
-  const result = parse(query)
+  const result = parseQuery(query)
   t.same(result.filters[0].args, [])
   t.end()
 })
 
 test("should extract all filter's colon-separated arguments as strings in order", (t) => {
   const query = '.home li > a | trim:left:right:both'
-  const result = parse(query)
+  const result = parseQuery(query)
   t.strictSame(result.filters[0].args[0], 'left')
   t.strictSame(result.filters[0].args[1], 'right')
   t.strictSame(result.filters[0].args[2], 'both')
