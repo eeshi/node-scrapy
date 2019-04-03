dc84695f7d5e8230d2280712d7c13b855ed05ef8
Stefan Maric <me@stefanmaric.com>
2019-04-03 20:42:54 +0200

    Add more tests for query-parser related to unicode and whitespace

--- test/query-parser.test.js
+++ test/query-parser.test.js
@@ -22,6 +22,13 @@ test('should extract CSS selector from query and keep it intact, even if getter 
   t.end()
 })
 
+test('should extract CSS selector from query and keep it intact, but trim whitespace', (t) => {
+  const query = '   .home li > a    ($ | trim:both)'
+  const result = parseQuery(query)
+  t.strictSame(result.selector, '.home li > a')
+  t.end()
+})
+
 test('should return null for `getter` if not present in the query', (t) => {
   const query = '.home li > a'
   const result = parseQuery(query)
@@ -65,15 +72,52 @@ test('should extract all filter names in order', (t) => {
   t.end()
 })
 
-test("should extract filters' argument as strings when quoted", (t) => {
-  const query = '.home li > a ($ | trim:"right" | anotherFilter:"test")'
+test("should extract filters' argument as strings when double quoted", (t) => {
+  const query = '.home li > a ($ | filter:"double" | anotherFilter:"quotes")'
   const result = parseQuery(query)
-  t.strictSame(result.filters[0].args[0], 'right')
-  t.strictSame(result.filters[1].args[0], 'test')
+  t.strictSame(result.filters[0].args[0], 'double')
+  t.strictSame(result.filters[1].args[0], 'quotes')
   t.end()
 })
 
-test("should extract filters' argument symbos as strings", (t) => {
+test("should extract filters' argument as strings when single quoted", (t) => {
+  const query = ".home li > a ($ | trim:'single' | anotherFilter:'quotes')"
+  const result = parseQuery(query)
+  t.strictSame(result.filters[0].args[0], 'single')
+  t.strictSame(result.filters[1].args[0], 'quotes')
+  t.end()
+})
+
+test('should preserve any unicode char when using quoted strings', (t) => {
+  const query = '.home li > a ($ | say:"helló világ":"こんにちは世界":"Привет, мир")'
+  const result = parseQuery(query)
+  t.strictSame(result.filters[0].args[0], 'helló világ')
+  t.strictSame(result.filters[0].args[1], 'こんにちは世界')
+  t.strictSame(result.filters[0].args[2], 'Привет, мир')
+  t.end()
+})
+
+test('should allow to escape single quotes inside of single quoted strings', (t) => {
+  // The double backslash is to escape the backslash symbol inside a JS string.
+  // The string bellow is actually:
+  // .home li > a ($ | say:'this: \' is an escaped quote')
+  const query = ".home li > a ($ | say:'this: \\' is an escaped quote')"
+  const result = parseQuery(query)
+  t.strictSame(result.filters[0].args[0], "this: ' is an escaped quote")
+  t.end()
+})
+
+test('should allow to escape double quotes inside of double quoted strings', (t) => {
+  // The double backslash is to escape the backslash symbol inside a JS string.
+  // The string bellow is actually:
+  // .home li > a ($ | say:"this: \" is an escaped quote")
+  const query = '.home li > a ($ | say:"this: \\" is an escaped quote")'
+  const result = parseQuery(query)
+  t.strictSame(result.filters[0].args[0], 'this: " is an escaped quote')
+  t.end()
+})
+
+test("should extract filters' argument symbols as strings", (t) => {
   const query = '.home li > a ($ | trim:right | anotherFilter:test)'
   const result = parseQuery(query)
   t.strictSame(result.filters[0].args[0], 'right')
